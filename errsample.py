6051f21fed8283b7c015644d7b17a04695653389
Satoru Baba <sbaba@jp.ibm.com>
2018-06-12 16:57:43 +0900

    test

--- errsample.py
+++ errsample.py
@@ -1,10 +1,10 @@
 from errbot import BotPlugin, botcmd, re_botcmd
-import re
-import sys
+#import re
+#import sys
 #import pprint
-import time
+#import time
 #from user_state import UserState
-from itertools import chain
+#from itertools import chain
 #import watson_developer_cloud
 #from assistant import Assistant
 
@@ -15,12 +15,12 @@ CONFIG_TEMPLATE = {'WORKSPACE_NAME': 'Car Dashborad - Sample',
 
 class errsample(BotPlugin):
     """Example plugin for Errbot"""
-    def __init__(self):
-        self.running = True
-        #
-        self.delay = 0.5  # second
-        #self.user_state_map = {}
-        #self.pp = pprint.PrettyPrinter(indent=4)
+    #def __init__(self):
+    #    self.running = True
+    #    #
+    #    self.delay = 0.5  # second
+    #    #self.user_state_map = {}
+    #    #self.pp = pprint.PrettyPrinter(indent=4)
 
     #def get_configuration_template(self):
     #    return CONFIG_TEMPLATE
@@ -37,10 +37,10 @@ class errsample(BotPlugin):
 
     #    super(PluginExample, self).configure(config)
 
+    #@re_botcmd(pattern=r"^[Ww][Aa][Tt][Ss][Oo][Nn] ",prefixed=False )
     @botcmd
     def watson(self, msg, args):
         return "Hello World!" 
 
 
-    #@re_botcmd(pattern=r"^[Ww][Aa][Tt][Ss][Oo][Nn] ",prefixed=False )
 
