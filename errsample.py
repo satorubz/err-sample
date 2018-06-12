c0d159f16da766151fa9be73148a6a2f72cb8186
Satoru Baba <sbaba@jp.ibm.com>
2018-06-12 16:52:11 +0900

    initial

---
+++ errsample.py
@@ -0,0 +1,46 @@
+from errbot import BotPlugin, botcmd, re_botcmd
+import re
+import sys
+#import pprint
+import time
+#from user_state import UserState
+from itertools import chain
+#import watson_developer_cloud
+#from assistant import Assistant
+
+CONFIG_TEMPLATE = {'WORKSPACE_NAME': 'Car Dashborad - Sample',
+                   'ASSISTANT_USERNAME': 'changeme',
+                   'ASSISTANT_PASSWORD': 'changeme',
+                   'ASSISTANT_VERSION_DATA': '2018-02-16' }
+
+class errsample(BotPlugin):
+    """Example plugin for Errbot"""
+    def __init__(self):
+        self.running = True
+        #
+        self.delay = 0.5  # second
+        #self.user_state_map = {}
+        #self.pp = pprint.PrettyPrinter(indent=4)
+
+    #def get_configuration_template(self):
+    #    return CONFIG_TEMPLATE
+
+    #def check_configuration(self, configuration):
+    #    pass
+
+    #def configure(self, configuration):
+    #    if configuration is not None and configuration != {}:
+    #        config = dict(chain(CONFIG_TEMPLATE.items(),
+    #                            configuration.items()))
+    #    else:
+    #        config = CONFIG_TEMPLATE
+
+    #    super(PluginExample, self).configure(config)
+
+    @botcmd
+    def watson(self, msg, args):
+        return "Hello World!" 
+
+
+    #@re_botcmd(pattern=r"^[Ww][Aa][Tt][Ss][Oo][Nn] ",prefixed=False )
+
